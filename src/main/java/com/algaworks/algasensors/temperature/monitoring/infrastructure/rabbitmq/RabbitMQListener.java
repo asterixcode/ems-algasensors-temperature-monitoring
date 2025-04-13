c477d4c73653e5dd65717ab113531a8fc9ba58bd
lucas.saldanha <lucas.saldanha@bestseller.com>
2025-04-13 20:22:04 +0200

    feat: enhance RabbitMQ listener with concurrency settings and simulated delay

--- src/main/java/com/algaworks/algasensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQListener.java
+++ src/main/java/com/algaworks/algasensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQListener.java
@@ -2,6 +2,7 @@ package com.algaworks.algasensors.temperature.monitoring.infrastructure.rabbitmq
 
 import com.algaworks.algasensors.temperature.monitoring.api.model.TemperatureLogData;
 import com.algaworks.algasensors.temperature.monitoring.domain.service.TemperatureMonitoringService;
+import java.time.Duration;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -15,8 +16,13 @@ public class RabbitMQListener {
     this.temperatureMonitoringService = temperatureMonitoringService;
   }
 
-  @RabbitListener(queues = RabbitMQConfig.QUEUE)
-  public void listen(@Payload TemperatureLogData temperatureLogData) {
+  @RabbitListener(
+      queues = RabbitMQConfig.QUEUE,
+      concurrency = "2-3") // Simulate a concurrency of 2 to 3
+  public void listen(@Payload TemperatureLogData temperatureLogData) throws InterruptedException {
     temperatureMonitoringService.processTemperature(temperatureLogData);
+
+    // Simulate a long-running task
+    Thread.sleep(Duration.ofSeconds(5));
   }
 }
